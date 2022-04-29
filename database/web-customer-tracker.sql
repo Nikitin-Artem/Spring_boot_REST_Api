4246f6e3cf8ce3b03481043ac91dccbad9ab3d03
Artem <Nikitin-Artem>
2022-04-30 02:25:32 +0300

    Initial commit

---
+++ database/web-customer-tracker.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE  IF NOT EXISTS spring_boot_rest_api;
+USE spring_boot_rest_api;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` VALUES 
+	(1,'David','Adams','david@mail.com'),
+	(2,'John','Doe','john@gmail.com'),
+	(3,'Ajay','Rao','ajay@gmail.com'),
+	(4,'Mary','Public','mary@yandex.com'),
+	(5,'Maxwell','Dixon','max@yandex.com');
+
