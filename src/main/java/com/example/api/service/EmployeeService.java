3794d2c033e6b47ffbcd2ed59af69be005a04381
Artem <Nikitin-Artem>
2022-04-30 03:21:05 +0300

    Add service layer

--- src/main/java/com/example/api/service/EmployeeService.java
+++ src/main/java/com/example/api/service/EmployeeService.java
@@ -1,4 +1,16 @@
 package com.example.api.service;
 
+import com.example.api.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    public List<Employee> getEmployees();
+
+    public Employee findById(int employeeId);
+
+    public void save(Employee employee);
+
+    public void deleteById(int employeeId);
 }
